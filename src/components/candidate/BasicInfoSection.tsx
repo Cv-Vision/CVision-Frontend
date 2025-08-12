517e40258f0f3a8b0f5be8bb66d49bc61b97797e
HiluLagos <hlagos@mail.austral.edu.ar>
2025-08-12 12:14:29 -0300

    View de Perfil de candidato actualizada Ahora incluye los campos nuevos utilizados en el registro

--- src/components/candidate/BasicInfoSection.tsx
+++ src/components/candidate/BasicInfoSection.tsx
@@ -6,14 +6,17 @@ import FormSection from "./FormSelection.tsx";
 interface Props {
     data: BasicInfo;
     onChange: (field: keyof BasicInfo, value: string) => void;
+    showPassword?: boolean;
 }
 
-const BasicInfoSection: FC<Props> = ({ data, onChange }) => {
+const BasicInfoSection: FC<Props> = ({ data, onChange, showPassword = true }) => {
     return (
         <FormSection title="Información Básica">
             <FormInput label="Nombre completo" value={data.fullName} onChange={(v) => onChange("fullName", v)} />
             <FormInput label="Correo electrónico" type="email" value={data.email} onChange={(v) => onChange("email", v)} />
-            <FormInput label="Contraseña" type="password" value={data.password} onChange={(v) => onChange("password", v)} />
+            {showPassword && (
+                <FormInput label="Contraseña" type="password" value={data.password} onChange={(v) => onChange("password", v)} />
+            )}
         </FormSection>
     );
 };
